5cc75b2ceabe9702f670b80a8a8cb2e33acbf3d4
dharanibaskaran <31499289+dharanibaskaran@users.noreply.github.com>
2018-01-19 16:08:26 +0530

    Create alphat.c

---
+++ alphat.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int ch;
+printf("enter the letter");
+scanf("%d",&ch);
+if(ch>='a'&&ch<='z')
+{
+printf("letterb is alphat");
+}
+else
+{
+printf("letter is not alphat");
+}
