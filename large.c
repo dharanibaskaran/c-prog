bf29923ebf0e026d1729486b8d0c40e8c0168c6e
dharanibaskaran <31499289+dharanibaskaran@users.noreply.github.com>
2018-01-19 16:40:13 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<coni.h>
+void main()
+{
+int a,b,c;
+printf("enter the number");
+scanf("%d%d%d",&a,&b,&c);
+if((a>=b)&&(a>=c))
+printf("a is largest number");
+if((b>=a)&&(b>c))
+printf("b is greater number");
+if((c>=a)&&(c>=b))
+}
