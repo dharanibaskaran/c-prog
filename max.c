486fe8aa58dff474020d8b4613efa5d073847961
dharanibaskaran <31499289+dharanibaskaran@users.noreply.github.com>
2018-01-28 15:41:10 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+  int main() {
+    int a[10];
+    int i;
+    int c;
+    printf("Enter ten values:");
+    for (i = 0; i < 10; i++) {
+    scanf("%d", &a[i]);
+    }
+    greatest = a[0];
+    for (i = 0; i < 10; i++) {
+if (a[i] > c) {
+c = a[i];
+    }
+    }
+    printf("enter c of ten numbers is %d", c);
+    return 0;
+  }
