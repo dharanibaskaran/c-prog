bab50ca57a8aa9d45fb94d90125f9bd059d12d10
dharanibaskaran <31499289+dharanibaskaran@users.noreply.github.com>
2018-01-28 15:33:07 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a = 65;
+    int b = 120;
+    printf(" value of a=%d b=%d before swapping", a, b);
+
+    a = a ^ k;
+    b = a ^ k;
+    a= a ^ k;
+    printf("value of i=%d k=%d after swapping", a, b);
+
+    return 0;
+}
