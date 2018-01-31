d3f9c99d690849e0face906a8676634cb730b8f3
dharanibaskaran <31499289+dharanibaskaran@users.noreply.github.com>
2018-01-31 15:57:50 +0530

    Create median.c

---
+++ median.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int c,d[10],i,sum=0;
+printf("Enter how many numbers \n");
+scanf("%d",&c);
+for(i=0;i<;i++)
+{
+scanf("%d",&d[i]);
+sum=sum+d[i];
+}
+int median=sum/c;
+printf("median is :%d\n",median);
+}
