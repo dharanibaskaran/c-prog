ad4bce4ce3294fa6898fef6e62976db40da64101
dharanibaskaran <31499289+dharanibaskaran@users.noreply.github.com>
2018-02-02 16:02:37 +0530

    Create same.c

---
+++ same.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,j,k,m,a,b;
+printf("enter the first time");
+scanf("%d%d",&i,&j);
+printf("enter the second time");
+scanf("%d%d",&k,&m);
+a=i-k;
+b=j-m;
+printf("the time is %d%d",a,b);
+return 0;
+}
