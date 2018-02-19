b16683d6f268edddd519c48de12271f811658059
dharanibaskaran <31499289+dharanibaskaran@users.noreply.github.com>
2018-02-19 16:39:50 +0530

    Update odd no2.c

--- odd no2.c
+++ odd no2.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 int main()
-
 {
-int counter;
-for(counter=1;counter<=100,counter+1)
-{
-if(counter%2==0)
-printf("%d",counter);
-}
-}
+ int i,n1,n2;
+ printf("enter the a value");
+ scanf("%d",&n1);
+ printf("enter the b value");
+ scanf("%d",&n2);
+ for(i=n1;i<=n2;i++)
+ {
+  if(i%2==0)
+   printf("%d",i);
+ }
