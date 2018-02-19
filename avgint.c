289c7676e0b417e5c91e354f55ee536284f4c962
dharanibaskaran <31499289+dharanibaskaran@users.noreply.github.com>
2018-02-19 16:49:35 +0530

    Create avgint.c

---
+++ avgint.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int b,n,sum=0,i;
+printf("enter the range");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+sum=sum+i;
+b=sum/n;
+}
+printf("average no %d",a);
+return 0;
+}
