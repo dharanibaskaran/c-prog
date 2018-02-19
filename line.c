7f04f5b563d22b2d7fb02d0c3eb41363ad4073a4
dharanibaskaran <31499289+dharanibaskaran@users.noreply.github.com>
2018-02-19 16:16:35 +0530

    Update line.c

--- line.c
+++ line.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
-    int b=123,n,sum=0;
-    while(b<=0)
-    {
-        n=a%10;
-        sum=sum+n;
-        printf("%d",sum)
-    }
+int a;
+printf("Enter the number:");
+scanf("%d",&a);
+printf("%d",a);
 }
 
