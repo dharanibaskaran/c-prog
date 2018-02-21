37e51891ae2f99f24d50859de07ff323b886a0d8
dharanibaskaran <31499289+dharanibaskaran@users.noreply.github.com>
2018-02-21 16:28:29 +0530

    Create print no.c

---
+++ print no.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+    int a,num=0;
+    printf("enter the i value");
+    scanf("%d",&a);
+    while(num!=0)
+    {
+        num=a+10;
+        printf("%d",num);
+        }
+switch(a)
+{
+    case 1:
+    printf("one");
+    break;
+    case 2:
+    printf("two");
+    break;
+    case 3:
+    printf("three");
+    break;
+}
+}
