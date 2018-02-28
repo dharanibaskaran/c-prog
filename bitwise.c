b8c5a68de23b639bb3e8db3ddabb080b0dd9cd25
dharanibaskaran <31499289+dharanibaskaran@users.noreply.github.com>
2018-02-28 16:19:02 +0530

    Create bitwise.c

---
+++ bitwise.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a=10,b=20,temp;
+    {
+        temp=x;
+        x=y;
+        y=temp;
+    }
+      printf("the x value is%d",x);
+      printf("the y value is%d",y );
+    }
