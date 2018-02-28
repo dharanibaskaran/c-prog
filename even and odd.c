678770f6b3808d9515f52c2d74d36b23b5650c81
dharanibaskaran <31499289+dharanibaskaran@users.noreply.github.com>
2018-02-28 15:52:51 +0530

    Create even and odd.c

---
+++ even and odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a=12,b=3,c;
+    d=a*b;
+    if(d%2==0)
+    {
+        printf("the number is even");
+    }
+else
+{
+    printf("the number is odd");
+}
+}
