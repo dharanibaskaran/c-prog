ed63aa9ccbf7a91f0ad0910e0c03c5b2a8f3b9bf
dharanibaskaran <31499289+dharanibaskaran@users.noreply.github.com>
2018-03-12 15:45:35 +0530

    Create nm.c

---
+++ nm.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a=68,b=23,g;
+    g=a+b;
+    if(g%2==0)
+    {
+        printf("given number is even");
+    }
+    else
+    {
+        printf("odd number");
+    }
+    }
