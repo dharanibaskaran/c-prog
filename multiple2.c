b9f930cdaa796c1f7895ea5806898b520920d3be
dharanibaskaran <31499289+dharanibaskaran@users.noreply.github.com>
2018-03-14 16:30:06 +0530

    Rename multiple.c to multiple2.c

---
+++ multiple2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the value");
+    scanf("%d",&n);
+    if(n%7==0)
+    {
+        printf("yes");
+    }
+
+    else
+    {
+        printf("no");
+    }
+}
